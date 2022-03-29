35b0aaef110b763b84a09f473073fd73c8cd10d1
Rafael Pelle <rafapelle@gmail.com>
2022-03-29 01:03:57 -0300

    static using SSG mode with fetch and revalidate

---
+++ src/pages/static5.tsx
@@ -0,0 +1,36 @@
+import styles from '../styles/Home.module.css';
+
+interface Props {
+  num: number;
+}
+
+export async function getStaticProps() {
+  let num;
+  try {
+    const response = await fetch('http://localhost:3000/api/random');
+    const json = await response.json();
+    num = json.num;
+  } catch (err) {
+    console.error(err);
+    num = 0;
+  }
+
+  return {
+    revalidate: 10, // In seconds
+    props: {
+      num,
+    },
+  };
+}
+
+export default function Static5({ num }: Props) {
+  return (
+    <div className={styles.container}>
+      <h1>This content is fully static</h1>
+      <p>
+        It will be rendered during the project build and rerendered every 10 sec
+      </p>
+      <p>{num}</p>
+    </div>
+  );
+}
